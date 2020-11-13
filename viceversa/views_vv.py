b791da25837a83ce8396bb6d7de58b8efa68e6dc
BobDoom <itsallover88@gmail.com>
2020-11-13 19:49:12 +0300

    Задание 3, добавлен счётчик слов.

--- viceversa/views_vv.py
+++ viceversa/views_vv.py
@@ -8,8 +8,13 @@ def home_vv(request):
 def reverse(request):
     user_text = request.GET['usertext']
     # print (user_text)
+    word_counter = len(user_text.split())  # Разделяет по пробелам по умолчанию (.split()) и записывает в list, считает количество элементов list.
     reversed_text = user_text[::-1]
-    if reversed_text.lower() == user_text.lower():
-        return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text, 'omonim': 'омоним.'})
-    else: 
-        return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text, 'omonim': 'простое скучное.'})
+
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text, 'wordcounter': word_counter}) 
+
+
+    # if reversed_text.lower() == user_text.lower():
+    #     return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text, 'omonim': 'омоним.'})
+    # else: 
+    #     return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text, 'omonim': 'простое скучное.'})
